b3fa1676ac8b4c711f186107e4464959b93665a9
AMAL-MURALI <117144014+AMAL-MURALI@users.noreply.github.com>
2023-07-21 21:14:34 +0530

    update

--- server/database/connection.js
+++ server/database/connection.js
@@ -4,10 +4,7 @@ const connectDB= async()=>{
     try {
         //mongodb connection string
 
-        const con =await mongoose.connect(process.env.MONGO_URI,{
-          
-            
-        })
+        const con =await mongoose.connect(process.env.MONGO_URI,{useNewUrlParser: true})
         
         console.log(`MongoDB connected:${con.connection.host}`);
     } catch (err) {
