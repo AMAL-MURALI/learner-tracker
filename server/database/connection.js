5f46e1903ffbdb14ca1ace525a6f5ebed31531fe
AMAL-MURALI <117144014+AMAL-MURALI@users.noreply.github.com>
2023-07-22 11:39:04 +0530

    change

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,15 +1,15 @@
 const mongoose=require('mongoose');
-const dotenv=require('dotenv').config()
+const dotenv=require('dotenv')
 const connectDB= async()=>{
     try {
         //mongodb connection string
-
+        dotenv.config({path:'./.env'})
         const con =await mongoose.connect(process.env.MONGO_URI,{useNewUrlParser: true})
         
         console.log(`MongoDB connected:${con.connection.host}`);
     } catch (err) {
         console.log(err);
-        process.exit(1);
+        
     }
 }
 
